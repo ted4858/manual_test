9ccab67950c23b0d0a9d159e256a243e639c0b90
ted4858 <72432477+ted4858@users.noreply.github.com>
2023-05-24 17:37:57 +0900

    Add files via upload

--- camera_class/camera.py
+++ camera_class/camera.py
@@ -2,32 +2,83 @@ from pypylon import pylon
 import platform
 from datetime import datetime
 
-def save_images(num_img_to_save):
-    img = pylon.PylonImage()
-    tlf = pylon.TlFactory.GetInstance()
+class ImageSaver:
+    def __init__(self):
+        self.img = pylon.PylonImage()
+        self.tlf = pylon.TlFactory.GetInstance()
+        self.cam = None
 
-    cam = pylon.InstantCamera(tlf.CreateFirstDevice())
-    cam.Open()
-    cam.StartGrabbing()
-    for i in range(num_img_to_save):
-        with cam.RetrieveResult(2000) as result:
+    def cam_open(self):
+        self.cam = pylon.InstantCamera(self.tlf.CreateFirstDevice())
+        self.cam.Open()
+        self.cam.StartGrabbing()
 
-            current_time = datetime.now().strftime("%Y%m%d_%H시%M분%S초")
+    def cam_close(self):
+        self.cam.StopGrabbing()
+        self.cam.Close()
 
-            img.AttachGrabResultBuffer(result)
+    def save_images(self, num_img_to_save, img_num):
+        for i in range(num_img_to_save):
+            with self.cam.RetrieveResult(2000) as result:
+                current_time = datetime.now().strftime("%Y%m%d_%H시%M분%S초")
+                self.img.AttachGrabResultBuffer(result)
 
-            if platform.system() == 'Windows':
+                if platform.system() == 'Windows':
+                    ipo = pylon.ImagePersistenceOptions()
+                    ipo.SetQuality(100)
+                    if num_img_to_save == 1:
+                        filename = "camera_img/{}_img_{}.jpeg".format(current_time, img_num)
+                        self.img.Save(pylon.ImageFileFormat_Jpeg, filename, ipo)
+                    else:
+                        filename = "camera_img/{}_img_{}_{}.jpeg".format(current_time, img_num, i)
+                        self.img.Save(pylon.ImageFileFormat_Jpeg, filename, ipo)
+                else:
+                    if num_img_to_save == 1:
+                        filename = "camera_img/{}_saved_pypylon_img_{}.png".format(current_time, img_num)
+                        self.img.Save(pylon.ImageFileFormat_Png, filename)
+                    else:
+                        filename = "camera_img/{}_saved_pypylon_img_{}.png".format(current_time, img_num, i)
+                        self.img.Save(pylon.ImageFileFormat_Png, filename)
 
-                ipo = pylon.ImagePersistenceOptions()
-                ipo.SetQuality(100)
+                self.img.Release()
 
-                filename = "camera_img/{}_img_{}.jpeg".format(current_time, i+1)
-                img.Save(pylon.ImageFileFormat_Jpeg, filename, ipo)
-            else:
-                filename = "camera_img/{}_saved_pypylon_img_{}.png".format(current_time, i+1)
-                img.Save(pylon.ImageFileFormat_Png, filename)
+# Usage:
+# saver = ImageSaver()
+# saver.cam_open()
+# for i in range(400):
+#     saver.save_images(1, i)
+# saver.cam_close()
 
-            img.Release()
+# from pypylon import pylon
+# import platform
+# from datetime import datetime
 
-    cam.StopGrabbing()
-    cam.Close()
+# def save_images(num_img_to_save):
+#     img = pylon.PylonImage()
+#     tlf = pylon.TlFactory.GetInstance()
+
+#     cam = pylon.InstantCamera(tlf.CreateFirstDevice())
+#     cam.Open()
+#     cam.StartGrabbing()
+#     for i in range(num_img_to_save):
+#         with cam.RetrieveResult(2000) as result:
+
+#             current_time = datetime.now().strftime("%Y%m%d_%H시%M분%S초")
+
+#             img.AttachGrabResultBuffer(result)
+
+#             if platform.system() == 'Windows':
+
+#                 ipo = pylon.ImagePersistenceOptions()
+#                 ipo.SetQuality(100)
+
+#                 filename = "camera_img/{}_img_{}.jpeg".format(current_time, i+1)
+#                 img.Save(pylon.ImageFileFormat_Jpeg, filename, ipo)
+#             else:
+#                 filename = "camera_img/{}_saved_pypylon_img_{}.png".format(current_time, i+1)
+#                 img.Save(pylon.ImageFileFormat_Png, filename)
+
+#             img.Release()
+
+#     cam.StopGrabbing()
+#     cam.Close()
--- test.py
+++ test.py
@@ -1,3 +1,45 @@
 from camera.camera_class import camera
+import time
 
-camera.save_images(1)
+# 고속카메라 이미지 저장하는 객체 생성 : image_saver
+image_saver = camera.ImageSaver()
+
+# 고속카메라 연결 함수 실행
+image_saver.cam_open()
+
+# 이미지 400개 저장(초당 196장, 실제 사용하는 프로그램에서 초당 100장 저장하도록 수정)
+for i in range(400):
+    # 이미지를 jpeg 파일로 저장하는 함수 실행
+    # 파일 이름이 중복되어 이미지가 1개만 저장되는 문제 해결하기 위해 파일 번호 추가
+    # save_images(파일 개수, 파일 번호)
+    # ex) image_saver.save_images(1, 2) -> 20230524_15시45분03초_img_2 와 같이 파일 1개 생성
+    # ex) image_saver.save_images(2, 2) -> 20230524_15시45분03초_img_2_1, 20230524_15시45분03초_img_2_2 와 같이 파일 2개 생성
+    image_saver.save_images(1, i)
+
+# 아래는 예시
+# 무한 반복문에서는 아래와 같이 사용하면 초당 100장 저장(약간의 오차 발생)
+###################################################################
+# last_time = time.perf_counter()
+# num_img_to_save = 1
+# img_num = 1
+
+# while True:
+#     image_saver.save_images(num_img_to_save, img_num)
+
+#     if img_num == 100:
+#         img_num = 1
+#         # break
+#     else:
+#         img_num += 1
+
+#     # sleep until 1 second has elapsed since the last loop
+#     elapsed_time = time.perf_counter() - last_time
+#     if elapsed_time < 0.01:
+#         time.sleep(0.01 - elapsed_time)
+#     else:
+#         time.sleep(0.01)
+#     last_time = time.perf_counter()
+###################################################################
+
+# 고속카메라 연결 종료 함수 실행
+image_saver.cam_close()
